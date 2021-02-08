a0e86dd16f9ccac0a677f2443b6fda1155e6ec77
Igor Prishchepov <igor.prishchepov@t-systems.com>
2021-02-08 11:33:58 +0300

    Adding elements to the list

---
+++ 3. algorithm/linkedlist.js
@@ -0,0 +1,80 @@
+class Node {
+    constructor(value) {
+      this.value = value;
+      this.previous = null;
+      this.next = null;
+    }
+  }
+
+  class LinkedList {
+
+    constructor() {
+      this.head = null;
+      this.tail = null;
+      this.length = 0;
+    }
+
+    isEmpty() {
+        return this.length === 0;
+    }
+
+    getNodeAtIndex(index) {
+        if (index < 0 || index >= this.length) {
+            throw new Error('index is outside of the list boundaries');
+        }
+        let currentNode = this.head;
+        let currentIndex = 0;
+        while (currentIndex !== index) {
+          currentNode = currentNode.next;
+          currentIndex++;
+        }
+        return currentNode;
+      }
+
+    addFirst(value) {
+        const newNode = new Node(value);
+        if (this.isEmpty()) {
+            this.head = newNode;
+            this.tail = newNode;
+        } else {
+            newNode.next = this.head;
+            this.head.previous = newNode;
+            this.head = newNode;
+        }
+        this.length++;
+    }
+
+    addLast(value) {
+        const newNode = new Node(value);
+        if (this.isEmpty()) {
+          this.head = newNode;
+          this.tail = newNode;
+        } else {
+          this.tail.next = newNode;
+          newNode.previous = this.tail;
+          this.tail = newNode;
+        }
+        this.length++;
+    }
+
+    insertAtIndex(index, value) {
+        if (index < 0 || index > this.length) {
+          throw new Error('index is outside of the list boundaries');
+        }
+
+        if (index === 0) {
+            this.addFirst(value);
+        } else if (index === this.length) {
+          this.addLast(value);
+        } else {
+            const newNode = new Node(value);
+            const currentNode = this.getNodeAtIndex(index);
+            const beforeCurrentNode = currentNode.previous;
+            currentNode.previous = newNode;
+            beforeCurrentNode.next = newNode;
+            newNode.next = currentNode;
+            newNode.previous = beforeCurrentNode;
+        }
+    }
+}
+  
